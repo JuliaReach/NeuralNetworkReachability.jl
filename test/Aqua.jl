372b99fe958e31c6a27ddf6c59bd6c09542ef9ae
schillic <git@christianschilling.net>
2024-10-15 22:48:42 +0200

    Aqua v0.8.9 tests only for local ambiguities

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -2,8 +2,5 @@ using NeuralNetworkReachability, Test
 import Aqua
 
 @testset "Aqua tests" begin
-    Aqua.test_all(NeuralNetworkReachability; ambiguities=false)
-
-    # do not warn about ambiguities in dependencies
-    Aqua.test_ambiguities(NeuralNetworkReachability)
+    Aqua.test_all(NeuralNetworkReachability)
 end
